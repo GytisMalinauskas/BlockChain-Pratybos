ed3d4d19a8f2188c63727b1041bf7acfc34c79f8
GytisMalinauskas <pvardenis30@gmail.com>
2024-09-25 17:14:12 +0300

    Patobulinimas

--- sources/function.cpp
+++ sources/function.cpp
@@ -7,18 +7,22 @@ string toHexString(const vector<uint8_t>& bytes) {
     }
     return ss.str();
 }
-// Paprasta maišos funkcija
+// Patobulinta maišos funkcija
 vector<uint8_t> simpleHash(const string& input) {
     // Fiksuotas išėjimo dydis - 256 bitai arba 32 baitai
     const size_t hashSize = 32;
     vector<uint8_t> hash(hashSize, 0); // Inicializuojame nulių masyvą
+    
+    // Papildomas maišymas su pradine verte
+    uint8_t salt = 0xAB;  // Paprastas "salt", kuris padeda maišyti pradines reikšmes
 
     for (size_t i = 0; i < input.length(); ++i) {
-        // Paprastas algoritmas: kiekvienas simbolis prisideda prie kiekvieno baito maišos
-        hash[i % hashSize] ^= input[i] + i;
-        // Maišymas: paslinkiame bito vietas kiekvienam baitui, kad simbolių pozicija turėtų įtakos
-        hash[i % hashSize] = ((hash[i % hashSize] << 5) | (hash[i % hashSize] >> 3));
+        // Kiekvienas simbolis prisideda prie kiekvieno baito maišos
+        hash[i % hashSize] ^= (input[i] + salt + i * 31); // Naudojame daugiau matematikos, kad įvestis maišytųsi geriau
+        // Daugiau maišymo: paslinkimas ir XOR
+        hash[i % hashSize] = ((hash[i % hashSize] << 5) | (hash[i % hashSize] >> 3)) ^ salt;
+        salt = (salt * 31) + input[i]; // Modifikuojame salt, kad įvestis daugiau maišytųsi
     }
-    
+
     return hash;
 }
